e827f830ce228cb504628a9b23ab7c85417ba525
pjv <ezelspinguin@gmail.com>
2015-07-06 21:27:24 +0200

    Revert "Json_object works now"
    This reverts commit f0d4cdb32f3ee11344041fae3284b31d8dc9cc16.

--- lib/model_project.dart
+++ lib/model_project.dart
@@ -1,32 +1,38 @@
 library test_arrays_binding.model_project;
 
 import 'package:polymer/polymer.dart';
+import 'package:dartson/dartson.dart';
 
-import "package:json_object/json_object.dart";
-
-abstract class ProjectInterface extends Observable {
+/** Not documented yet. */
+@Entity()
+class Project extends Observable{
   /** Not documented yet. */
+  @Property(ignore:true)
   @observable String hash;
 
   /** Not documented yet. */
+  @Property(ignore:true)
   @observable String name = "New Project";
 
-  /** Not documented yet. */
+  String names;
+
+  @Property(ignore:true)
   @observable List<Category> categories = toObservable([]);
-}
+  /** Not documented yet. */
+  /*List<Category> _categories = toObservable([]);
+  List<Category> get categories => _categories;
+  void set categories(List<Category> value) {
+    this._categories = notifyPropertyChange(const Symbol('categories'), this._categories, toObservable(value));
+  }*/
 
+  //Project(this.hash, this.name, this.categories);
 
-/** Not documented yet. */
-class Project extends JsonObject implements ProjectInterface{
+  //Project.create(hash) : hash = hash, name = "New Project", categories = [];
 
-  Project();
-
-  Project.full(this.hash, this.name, this.categories);
-
-  Project.create(hash) : hash = hash, name = "New Project",categories = toObservable([]);
+  Project(): names = "bla";
 
   factory Project.fromJsonString(string){
-    Project p = new JsonObject.fromJsonString(string, new Project());
+    Project p = new Dartson.JSON().map({"names":"test"}, new Project());
     return p;
   }
 
@@ -35,6 +41,7 @@ class Project extends JsonObject implements ProjectInterface{
 }
 
 /** Not documented yet. */
+@Entity()
 class Category extends Observable{
   /** Not documented yet. */
   final int id;
@@ -43,12 +50,17 @@ class Category extends Observable{
   String name = "New category";
 
   /** Not documented yet. */
+  @Property(ignore:true)
   @observable List<String> subcategories = toObservable([]);
 
   Category(this.id, this.name, this.subcategories);
   Category.create() : id = null, name = "New", subcategories = toObservable([]);
 
   Category.empty();
+
+  factory Category.fromJsonString(string){
+    return new Dartson.JSON().map(string, new Category.empty());
+  }
 
   toString() => name;
 
--- lib/pane_edit.dart
+++ lib/pane_edit.dart
@@ -5,6 +5,8 @@ import 'dart:html';
 import 'dart:js';
 import 'package:firebase/firebase.dart';
 
+import 'package:dartson/dartson.dart';
+
 @CustomTag("pane-edit")
 class PaneEdit extends PolymerElement {
   PaneEdit.created() : super.created() {
@@ -13,11 +15,17 @@ class PaneEdit extends PolymerElement {
 //    project = toObservable(new Project.create("bla"));
 //    project.categories.add(toObservable(cat));
 
+
+
+    EntityClass object = new EntityClass.fromJson('{"name":"test","renamed":true,"notVisible":"it is", "setted": "awesome"}');
+
+
+
     Firebase dbRef = new Firebase("https://shining-heat-1634.firebaseio.com/");
 
     dbRef.child("project").onValue.listen((e) {
-      String val = e.snapshot.valAsJson();
-      Project projectb = new Project.fromJsonString(val);
+      Map val = e.snapshot.val();
+      project = new Project.fromJsonString(val);
     });
   }
 
@@ -46,4 +54,37 @@ class PaneEdit extends PolymerElement {
     project.categories[category_nr].subcategories.add(toObservable("subcatNew"));
   }
 
+}
+
+
+@Entity()
+class EntityClass extends Observable{
+  //@Property(name:"name")
+  //@observable String name;
+  String _setted;
+
+  @Property(name:"renamed")
+  bool otherName;
+
+  @Property(ignore:true)
+  String notVisible;
+
+  List<EntityClass> children;
+
+  set setted(String s) => _setted = s;
+  String get setted => _setted;
+
+  String _name;
+  String get name => _name;
+  void set name(String value) {
+    this._name = notifyPropertyChange(const Symbol('name'), this._name, value);
+  }
+
+  EntityClass();
+
+  factory EntityClass.fromJson(string){
+    var dson = new Dartson.JSON();
+    EntityClass object = dson.decode(string, new EntityClass());
+    return object;
+  }
 }
